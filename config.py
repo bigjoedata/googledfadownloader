55ae11b8b11b2b3c2d4c4cf2ec19388a23bcb106
bigjoedata <blechman@gmail.com>
2019-04-15 18:47:52 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+access_token = "AAAAAA"
+refresh_token = "BBBBBBB"
+client_id = "ZZZZZZ.apps.googleusercontent.com"
+client_secret = "YYYYY"
+profile_id="123456789"
+report_id="123456789"
